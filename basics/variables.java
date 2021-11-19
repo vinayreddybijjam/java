97228671971066e05f2d9b2f79a1151b67407c26
Your Name <you@example.com>
2021-11-19 17:37:56 +0530

    okie

--- basics/variables.java
+++ basics/variables.java
@@ -1,12 +1,13 @@
-public class variables {
-    class A{
-        int data=50;        //instance variable
-        static int m=100;   //static variable
-      void method() {       //local variable
-          int n=90;
-          
-      }
-          
-      }
-    }
+public class variables
+{  
+    static int m=100;//static variable  
+    void method()  
+    {    
+        int n=90;//local variable    
+    }  
+    public static void main(String args[])  
+    {  
+        int data=50;//instance variable    
+    }  
+}//end of class  
     
